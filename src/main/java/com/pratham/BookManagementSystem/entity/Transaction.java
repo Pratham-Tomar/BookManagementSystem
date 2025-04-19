e05ddf2368e32e6776f7a45cd46c2e1959b59350
Pratham Tomar <prathamtomar02@gmail.com>
2025-04-19 19:24:15 +0530

    added transaction

--- src/main/java/com/pratham/BookManagementSystem/entity/Transaction.java
+++ src/main/java/com/pratham/BookManagementSystem/entity/Transaction.java
@@ -1,12 +1,41 @@
 package com.pratham.BookManagementSystem.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.pratham.BookManagementSystem.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.mapping.Column;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Transaction {
     @Id
     private long transactionId;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "bookId")
+    private Book book;
+    private LocalDate borrowDate;
+    private LocalTime borrowTime;
+    private LocalDate returnDate;
+    private LocalTime returnTime;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    private double fineAmount;
+    private String remarks;
+
+
+
+
+
 
 
 }
--- src/main/java/com/pratham/BookManagementSystem/enums/Status.java
+++ src/main/java/com/pratham/BookManagementSystem/enums/Status.java
@@ -3,10 +3,8 @@ package com.pratham.BookManagementSystem.enums;
 public enum Status {
     BORROWED,
     RETURNED,
-    PENDING,
     OVERDUE,
     DAMAGED,
     LOST,
-    RESERVED,
 
 }
