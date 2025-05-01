2e5ed2b11f5a08f1b951dbbb7831bab8ff54aa78
Pratham Tomar <prathamtomar02@gmail.com>
2025-05-01 18:28:10 +0530

    added cahnges for the controller of the Transaction

---
+++ src/main/java/com/pratham/BookManagementSystem/controller/TransactionController.java
@@ -0,0 +1,42 @@
+package com.pratham.BookManagementSystem.controller;
+
+import com.pratham.BookManagementSystem.entity.Transaction;
+import com.pratham.BookManagementSystem.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Transaction")
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @GetMapping("/getAllTransaction")
+    public ResponseEntity<List<Transaction>> getAllTransaction() {
+        return ResponseEntity.ok(transactionService.getAllTransactions());
+    }
+
+    @GetMapping("/getTransactionById/{transactionId")
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable  long transactionId) {
+        return ResponseEntity.ok(transactionService.getTransactionById(transactionId));
+    }
+
+    @PostMapping("/borrowBook/{userId}/{bookId}")
+    public ResponseEntity<Transaction> borrowBook(@PathVariable int userId, @PathVariable int bookId,
+                                                  @RequestBody String remarks) {
+        return ResponseEntity.ok(transactionService.borrowBook(userId, bookId, remarks));
+    }
+
+    @PostMapping("/returnBook/{userId}/{bookId}")
+    public ResponseEntity<Transaction> returnBook(@PathVariable int userId, @PathVariable int bookId,
+                                                  @RequestBody String remarks) {
+        return ResponseEntity.ok(transactionService.returnBook(userId, bookId, remarks));
+    }
+
+
+
+}
