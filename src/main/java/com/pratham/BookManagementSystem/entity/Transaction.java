50ff2513c6969c62f35174ce1f61374316902647
Pratham Tomar <prathamtomar02@gmail.com>
2025-05-01 16:04:23 +0530

    added the transaction details with initialisation

--- src/main/java/com/pratham/BookManagementSystem/entity/Transaction.java
+++ src/main/java/com/pratham/BookManagementSystem/entity/Transaction.java
@@ -32,10 +32,96 @@ public class Transaction {
     private double fineAmount;
     private String remarks;
 
+    Transaction(){}
 
+    public Transaction(long transactionId, LocalDate borrowDate, LocalTime borrowTime, LocalDate returnDate, LocalTime returnTime, Status status, double fineAmount, String remarks) {
+        this.transactionId = transactionId;
+        this.borrowDate = borrowDate;
+        this.borrowTime = borrowTime;
+        this.returnDate = returnDate;
+        this.returnTime = returnTime;
+        this.status = status;
+        this.fineAmount = fineAmount;
+        this.remarks = remarks;
+    }
 
+    public long getTransactionId() {
+        return transactionId;
+    }
 
+    public void setTransactionId(long transactionId) {
+        this.transactionId = transactionId;
+    }
 
+    public User getUser() {
+        return user;
+    }
 
+    public void setUser(User user) {
+        this.user = user;
+    }
 
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public LocalDate getBorrowDate() {
+        return borrowDate;
+    }
+
+    public void setBorrowDate(LocalDate borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+
+    public LocalTime getBorrowTime() {
+        return borrowTime;
+    }
+
+    public void setBorrowTime(LocalTime borrowTime) {
+        this.borrowTime = borrowTime;
+    }
+
+    public LocalTime getReturnTime() {
+        return returnTime;
+    }
+
+    public void setReturnTime(LocalTime returnTime) {
+        this.returnTime = returnTime;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public double getFineAmount() {
+        return fineAmount;
+    }
+
+    public void setFineAmount(double fineAmount) {
+        this.fineAmount = fineAmount;
+    }
 }
